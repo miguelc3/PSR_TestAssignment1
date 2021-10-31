0bb35f53b698e5a6fc71d985ae969657f9196487
joaodmatias <joaodmatias@ua.pt>
2021-10-31 16:01:28 +0000

    I changed a few things including making argparse and the help message work. I still have to make the stats part for the max inputs mode.

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 # Import packages
-from colorama import Fore, Style
+from colorama import Fore, Style, Back
 import argparse
 import random
 import string
@@ -11,23 +11,29 @@ from pprint import pprint
 from interruptingcow import timeout # install this package
 
 
-def time_mode(t):
+parser = argparse.ArgumentParser(description='Definition of ' + Fore.LIGHTBLUE_EX + 'test ' + Style.RESET_ALL + 'mode')
+parser.add_argument('-utm', '--use_time_mode', action='store_true', help='')
+parser.add_argument('-mv', '--max_value', type=int, required=False, help=' Max number of secs for time mode or maximum number of inputs for number of inputs mode.')
+args = vars(parser.parse_args())
 
-    random_letters = []
-    pressed_keys = []
-    types = []
-    duration_types = []
+#print('Welcome to our typing test!\nPress any key to start the test.')
 
+random_letters = []
+pressed_keys = []
+types = []
+duration_types = []
+
+
+def time_mode():
     initial_time = time.time()
     total_hits = 0
     total_types = 0
-
-    print('Test running up to ' + str(t) + ' seconds')
+    print('Test running up to ' + str(args['max_value']) + ' seconds')
     print('Press any key yo start')
     readchar.readkey()
 
     try:
-        with timeout(t, exception=RuntimeError):
+        with timeout(args['max_value'], exception=RuntimeError):
             while True:
                 start_time = time.time()
 
@@ -55,10 +61,10 @@ def time_mode(t):
                 types.append(result)
 
     except RuntimeError:
-        print("You're time is over.")
+        print("Your time is over.")
         pass
 
-    type_average_duration = t/total_types
+    type_average_duration = (args['max_value'])/total_types
 
     total_hits_time = 0
     total_miss_time = 0
@@ -77,7 +83,7 @@ def time_mode(t):
                     'Inputs': types,
                     'Number of hints': total_hits,
                     'Number of types': total_types,
-                    'Test duration': str(t) + 's',
+                    'Test duration': str(args['max_value']) + 's',
                     'Test end': time.ctime(final_time),
                     'Test start': time.ctime(initial_time),
                     'Type average duration': type_average_duration,
@@ -87,10 +93,31 @@ def time_mode(t):
 
     pprint(dict_results)
 
+def max_inputs():
+    max_nletters = args['max_value']
+    start_key = readchar.readkey()
+    if start_key:
+        counter = 0
+        while True:
+            random_letter = random.choice(string.ascii_lowercase)
+            random_letters.append(random_letter)
+            print('Type ' + Back.LIGHTRED_EX + str(random_letter) + Style.RESET_ALL)
+            pressed_key = readchar.readkey()
+            if pressed_key == random_letter:
+                counter += 1
+                print('You typed ' + pressed_key + Fore.LIGHTGREEN_EX + ' -->RIGHT' + Style.RESET_ALL)
+            else:
+                counter += 1
+                print('You typed ' + pressed_key + Fore.RED + ' -->WRONG' + Style.RESET_ALL)
+            if counter==max_nletters:
+                break
 
 def main():
-    time_mode(10)
 
+    if args['use_time_mode'] is True:
+        time_mode()
+    else:
+        max_inputs()
 
 if __name__ == '__main__':
     main()
